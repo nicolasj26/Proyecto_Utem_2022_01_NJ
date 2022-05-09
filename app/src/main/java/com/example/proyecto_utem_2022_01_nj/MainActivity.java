fba56f4b1efb5098a13c2f17eb33628499dddf5c
nicolasj26 <nicolas.jimenezj@utem.cl>
2022-05-09 04:58:18 -0400

    Primer proyecto de computacion movil UTEM

---
+++ app/src/main/java/com/example/proyecto_utem_2022_01_nj/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.proyecto_utem_2022_01_nj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
